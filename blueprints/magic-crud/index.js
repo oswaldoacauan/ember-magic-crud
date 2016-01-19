6d8ee791756eed2e98d5960e4d5d8f4b5abe12f1
gcampes <gabrieldecampes@gmail.com>
2016-01-19 14:16:25 -0200

    Add blueprint por support

--- blueprints/magic-crud/index.js
+++ blueprints/magic-crud/index.js
@@ -1,14 +1,64 @@
+var path = require('path');
+
+var getPathOption = function(options){
+  var outputPath     = 'components';
+  if (options.path) {
+    outputPath = options.path;
+  } else {
+    if (options.path === '') {
+      outputPath = '';
+    }
+  }
+  return outputPath;
+}
+
 module.exports = {
-  description: ''
+  description: '',
 
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
-  // afterInstall: function(options) {
-  //   // Perform extra work here.
-  // }
+  fileMapTokens: function() {
+    return {
+      __controllerpath__: function(options) {
+        if (options.pod) {
+          return path.join(options.podPath, options.dasherizedModuleName);
+        }
+        return 'controllers';
+      },
+      __routepath__: function(options) {
+        if (options.pod) {
+          return path.join(options.podPath, options.dasherizedModuleName);
+        }
+        return 'routes';
+      },
+      __routename__: function(options) {
+        if (options.pod) {
+          return 'route';
+        }
+        return options.dasherizedModuleName;
+      },
+       __controllername__: function(options) {
+         if (options.pod) {
+           return 'controller';
+         }
+         return options.dasherizedModuleName;
+       },
+       __addroutename__: function(options) {
+         if (options.pod) {
+           return 'add/route';
+         }
+         return path.join(options.dasherizedModuleName, 'add');
+       },
+       __editroutename__: function(options) {
+         if (options.pod) {
+           return 'edit/route';
+         }
+         return path.join(options.dasherizedModuleName, 'edit');
+       },
+       __showroutename__: function(options) {
+         if (options.pod) {
+           return 'show/route';
+         }
+         return path.join(options.dasherizedModuleName, 'show');
+       }
+    };
+  }
 };
