2b1ccf4a349f7792b967fe72f66d3c9899a1bf6b
gcampes <gabrieldecampes@gmail.com>
2016-01-20 11:36:37 -0200

    Fix typo in MagicRouteMapper

--- addon/utils/magic-route-mapper.js
+++ addon/utils/magic-route-mapper.js
@@ -2,9 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Object.extend({
   map: function(context, route){
-  return context.route(route, function() {
-    context.route('add');
-    context.route('edit', {path:'edit/:id'});
-    context.route('show', {path:'show/:id'})
-  });
+    return context.route(route, function() {
+      context.route('add');
+      context.route('edit', {path:'edit/:id'});
+      context.route('show', {path:'show/:id'})
+    });
+  }
 }
